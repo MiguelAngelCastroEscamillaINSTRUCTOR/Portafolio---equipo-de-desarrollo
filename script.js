bc9d365908686bb02620910a1e8118e463a9423c
MiguelAngelCastroEscamillaINSTRUCTOR <macastro40@misena.edu.co>
2022-08-29 17:15:54 -0500

    Finalizado

---
+++ script.js
@@ -0,0 +1,43 @@
+// Funcion que me aplica el estilo a la opcion seleccionada en el menu y quita la  opcion seleccionada 
+function seleccionar(link){
+    var opciones = document.querySelectorAll('#links a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "seleccionado";
+
+    // hacemos desaparecer el menu uan vez que se ha selecionado una opcion en el modo responsive
+    var x = document.getElementById("nav");
+    x.className = ""; 
+}
+
+
+// function que muestra el menu responsive
+function responsiveMenu(){
+    var x = document.getElementById("nav");
+    if(x.className ===  ""){
+        x.className = "responsive";
+    }
+    else{
+        x.className = "";
+    }
+}
+
+// detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades()
+};
+
+// Funcion que aplica el tiempo a la barra de habilidades
+function efectoHabilidades (){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight = skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        document.getElementById("html").classList.add("barra-progreso1");
+        document.getElementById("js").classList.add("barra-progreso2");
+        document.getElementById("bd").classList.add("barra-progreso3");
+        document.getElementById("ps").classList.add("barra-progreso4");
+    }
+}
